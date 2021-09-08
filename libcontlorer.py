ba96ae72c93736a3f59ae195f75cc0c8618ab402
PhysKen <62343651+phys-ken@users.noreply.github.com>
2021-09-09 01:10:15 +0900

    微修正

--- libcontlorer.py
+++ libcontlorer.py
@@ -1,19 +1,17 @@
-from os import pathconf_names
+import os
 import subprocess
 
 # 相対パスのリストを作っておく
 paths = [
-"004/004_1_練習問題まで.mp4",
-"004/004_2練習問題解説.mp4",
-"004/004_3電荷の公式_Trim.mp4",
-"004/004_4点電荷の場合.mp4",
-"004/004_5例題1.mp4",
-"004/004_06_例題2.mp4"
+"input_mov/007_1_公式解説.mp4",
+"input_mov/007_2_電場と電位.mp4",
+"input_mov/007_3_高さ.mp4",
+"input_mov/007_4_練習問題.mp4"
 ]
 
 
 for p in paths:
-  subprocess.run(["python" , "handler/douga_auto_trimer.py" , p])
+  subprocess.run(["python" , "douga_auto_trimer.py" , p])
 
 print("以下の動画の処理が終わりました！！！********************")
 print(paths)
